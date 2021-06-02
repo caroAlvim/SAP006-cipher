0c56fc19135f08795a84efc6496c679a0e7a4a2a
Carolina Alvim <caro.alvim@gmail.com>
2021-06-02 17:40:41 -0300

    meu primeiro commit

--- src/cipher.js
+++ src/cipher.js
@@ -1,5 +1,12 @@
+
+
 const cipher = {
-  // ...
+  //...
 };
+
+
+
+// colocar toda a logica do programa
+// duas funçoes: 1 - encriptar e 2- decriptar a msg
 
 export default cipher;
--- src/index.js
+++ src/index.js
@@ -1,3 +1,40 @@
 import cipher from './cipher.js';
 
 console.log(cipher);
+
+
+const pegarTexto = (evento)=>{
+
+    evento.preventDefault()
+  
+    const textoUsuario =  document.querySelector('[data-form-input]')
+    const valor = textoUsuario.value 
+    
+    const fraseCifrada = document.querySelector('[data-cifra')
+    const conteudo = `<p class="content">${valor}</p>`
+  
+    fraseCifrada.innerHTML = conteudo
+  
+    textoUsuario.value = " "
+  
+};
+  
+const decifrarTexto = ()=>{
+  
+}
+
+const cifra = document.querySelector('[data-form-button]')
+const decifra = document.querySelector('[data-form-decode]')
+
+cifra.addEventListener('click', pegarTexto)
+
+decifra.addEventListener('clck', decifrarTexto)
+
+
+//let frase = prompt ("escreva aqui a sua msg para ser cifrada")
+//colocar toda a interação entre o js e html 
+// importar as funçoes
+//printar as msgs cifradas (domumentGetElement ?)
+// printar as msgs decifradas
+
+// manipulaçao de DOM
